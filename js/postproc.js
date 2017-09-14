1095207bf48f782540d9a049c448089cb7610150
Nutiler <tzh.nut@gmail.com>
2017-09-14 05:14:23 +0000

    Oh squares.

---
+++ js/postproc.js
@@ -0,0 +1,167 @@
+// Post Processing.
+// josh@stormheart.net
+
+// rStats.js - http://spite.github.io/rstats/
+var rS = new rStats({
+	CSSPath: './assets/stats/',
+	values: {
+		raf: { caption: 'rAF (ms)' },
+		frame: { caption: 'Frame Time', over: 16 },
+		fps: { caption: 'Framerate', below: 30 }
+	}
+});
+
+// WAGNER Shaders - https://github.com/spite/Wagner
+WAGNER.vertexShadersPath = './assets/shaders/vertex-shaders';
+WAGNER.fragmentShadersPath = './assets/shaders/fragment-shaders';
+WAGNER.assetsPath = './assets/';
+
+// 
+var gui;
+var depthTexture, sL;
+var depthMaterial = new THREE.MeshBasicMaterial();
+var rgbSplitPass, noisePass, vignette2Pass,
+	bloomPass, glowTexture, dofPass, fxaaPass;
+
+var glowMaterial = new THREE.MeshBasicMaterial({
+	map: tloader.load('./assets/textures/1324-glow.jpg'),
+});
+
+glowMaterial.map.repeat = new THREE.Vector2(1, 1);
+glowMaterial.map.wrapS = glowMaterial.map.wrapT = THREE.RepeatWrapping;
+
+function postProcessing() {
+	composer = new WAGNER.Composer(renderer, { useRGBA: false });
+
+	// Color Split
+	rgbSplitPass = new WAGNER.RGBSplitPass();
+	rgbSplitPass.params.delta.x = 30;
+	rgbSplitPass.params.delta.y = 15;
+
+	// Noise Static
+	noisePass = new WAGNER.NoisePass();
+	noisePass.params.speed = 1000;
+	noisePass.params.amount = 0.2;
+
+	// Brightening Vignette
+	vignette2Pass = new WAGNER.Vignette2Pass();
+	vignette2Pass.params.boost = 2;
+	vignette2Pass.params.reduction = 2;
+
+	// Bloom
+	bloomPass = new WAGNER.MultiPassBloomPass();
+	bloomPass.params.strength = .5;
+	bloomPass.params.blurAmount = 1.2;
+	bloomPass.params.applyZoomBlur = true;
+	bloomPass.params.zoomBlurStrength = .04;
+	// bloomPass.params.useTexture =true;
+
+	// Depth of Feild
+	dofPass = new WAGNER.GuidedFullBoxBlurPass();
+	dofPass.params.from = 0.1;
+	dofPass.params.to = 0;
+	dofPass.params.amount = 10;
+
+	// FXAA
+	fxaaPass = new WAGNER.FXAAPass();
+
+	// GUI - https://github.com/dataarts/dat.gui
+	gui = new dat.GUI();
+
+	gui.add(rgbSplitPass.params.delta, 'x').min(0).max(100);
+	gui.add(rgbSplitPass.params.delta, 'y').min(0).max(100);
+
+	gui.add(noisePass.params, 'amount').min(0).max(2);
+	gui.add(noisePass.params, 'speed').min(0).max(1000);
+
+	gui.add(vignette2Pass.params, 'boost').min(0).max(10);
+	gui.add(vignette2Pass.params, 'reduction').min(0).max(10);
+
+	gui.add(bloomPass.params, 'blurAmount').min(0).max(2);
+	gui.add(bloomPass.params, 'applyZoomBlur');
+	gui.add(bloomPass.params, 'zoomBlurStrength').min(0).max(2);
+	gui.add(bloomPass.params, 'useTexture');
+
+	gui.add(dofPass.params, 'from').min(0).max(1);
+	gui.add(dofPass.params, 'to').min(0).max(1);
+	gui.add(dofPass.params, 'amount').min(0).max(100);
+	gui.add(dofPass.params, 'invertBiasMap');
+
+	gui.open(); // or open
+
+	sL = new ShaderLoader()
+	sL.add('depth-vs', './assets/shaders/vertex-shaders/packed-depth-vs.glsl');
+	sL.add('depth-fs', './assets/shaders/fragment-shaders/packed-depth-fs.glsl');
+	sL.load();
+	sL.onLoaded(function() {
+		depthMaterial = new THREE.ShaderMaterial({
+			uniforms: {
+				mNear: { type: 'f', value: camera.near },
+				mFar: { type: 'f', value: camera.far }
+			},
+			vertexShader: this.get('depth-vs'),
+			fragmentShader: this.get('depth-fs'),
+			flatShading: THREE.SmoothShading
+		});
+	});
+	
+	createCubes();
+	scene.children[3].material = new THREE.MeshNormalMaterial();
+}
+
+function resizePass() {
+	composer.setSize(renderer.domElement.width, renderer.domElement.height);
+	bloomPass.params.zoomBlurCenter.set(.5 * composer.width, .5 * composer.height);
+	glowTexture = WAGNER.Pass.prototype.getOfflineTexture(composer.width, composer.height, false);
+	depthTexture = WAGNER.Pass.prototype.getOfflineTexture(composer.width, composer.height, false);
+}
+
+function renderPass() {
+	// stats.begin();
+	rS('FPS').frame();
+	rS('rAF').tick();
+
+	// depthMaterial.side = THREE.DoubleSide;
+	// scene.overrideMaterial = depthMaterial;
+
+	composer.reset();
+
+	depthMaterial.side = THREE.DoubleSide;
+	scene.overrideMaterial = depthMaterial;
+
+	composer.render(scene, camera, null, depthTexture);
+	dofPass.params.tBias = depthTexture;
+	scene.overrideMaterial = null;
+
+	if (bloomPass.params.useTexture) {
+		
+		scene.overrideMaterial = glowMaterial;
+		composer.render(scene, camera, null, glowTexture);
+
+		scene.overrideMaterial = null;
+		composer.render(scene, camera);
+
+		bloomPass.params.glowTexture = glowTexture;
+
+	}
+	else {
+		
+		composer.render(scene, camera);
+
+	}
+
+	composer.pass(fxaaPass);
+	composer.pass(dofPass);
+	composer.pass(bloomPass);
+
+	composer.pass(vignette2Pass);
+	composer.pass(rgbSplitPass);
+	composer.pass(noisePass);
+
+// console.log("ayy")
+	composer.toScreen();
+	
+	rS().update();
+	// stats.end();
+
+}
--- js/resize.js
+++ js/resize.js
@@ -1,45 +1,132 @@
 // Enter Fullscreen
-// var c = document.body;
-// document.getElementById( 'fullscreenBtn' ).addEventListener( 'click', function( e ) {
-// 	c.onwebkitfullscreenchange = function(e) {
-// 		c.onwebkitfullscreenchange = function() {
-// 		};
-// 	};
-// 	c.onmozfullscreenchange = function(e) {
-// 		c.onmozfullscreenchange = function() {
-// 		};
-// 	};
-// 	if( c.webkitRequestFullScreen ) c.webkitRequestFullScreen();
-// 	if( c.mozRequestFullScreen ) c.mozRequestFullScreen();
-// 	e.preventDefault();
-// }, false );
+var c = document.body;
+document.getElementById('fullscreenBtn').addEventListener('click', function(e) {
+	c.onwebkitfullscreenchange = function(e) {
+		c.onwebkitfullscreenchange = function() {};
+	};
+	c.onmozfullscreenchange = function(e) {
+		c.onmozfullscreenchange = function() {};
+	};
+	if (c.webkitRequestFullScreen) c.webkitRequestFullScreen();
+	if (c.mozRequestFullScreen) c.mozRequestFullScreen();
+	e.preventDefault();
+}, false);
 
-window.addEventListener( 'load', function() {
-	init();
-} );
+window.addEventListener('load', function() {
+	var blocker = document.getElementById('blocker');
+	var instructions = document.getElementById('instructions');
 
-function onMouseWheel( event ) {
+	// http://www.html5rocks.com/en/tutorials/pointerlock/intro/
 
-	// WebKit
-	if ( event.wheelDeltaY ) {
-		fov -= event.wheelDeltaY * 0.05;
-		
-	// Opera / Explorer 9
-	} else if ( event.wheelDelta ) {
-		fov -= event.wheelDelta * 0.05;
-		
-	// Firefox
-	} else if ( event.detail ) {
-		fov += event.detail * 1.0;
+	var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+
+	if (havePointerLock) {
+
+		var element = document.body;
+
+		var pointerlockchange = function(event) {
+
+			if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
+
+				controlsEnabled = true;
+				controls.enabled = true;
+
+				blocker.style.display = 'none';
+
+			}
+			else {
+
+				controls.enabled = false;
+
+				blocker.style.display = '-webkit-box';
+				blocker.style.display = '-moz-box';
+				blocker.style.display = 'box';
+
+				instructions.style.display = '';
+
+			}
+
+		};
+
+		var pointerlockerror = function(event) {
+
+			instructions.style.display = '';
+
+		};
+
+		// Hook pointer lock state change events
+		document.addEventListener('pointerlockchange', pointerlockchange, false);
+		document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+		document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
+
+		document.addEventListener('pointerlockerror', pointerlockerror, false);
+		document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+		document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
+
+		instructions.addEventListener('click', function(event) {
+
+			instructions.style.display = 'none';
+
+			// Ask the browser to lock the pointer
+			element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+			element.requestPointerLock();
+
+		}, false);
+
+	}
+	else {
+
+		instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
+
 	}
 
-	camera.updateProjectionMatrix();
-	
-}
 
-var mouseX = 0, mouseY = 0;
-function onDocumentMouseMove( e ) {
-	mouseX = 10 * ( .5 * window.innerWidth - e.pageX );
-	mouseY = 10 * ( .5 * window.innerHeight - e.pageY );
+
+	initialized();
+	animate();
+
+
+});
+
+
+// function onMouseWheel(event) {
+// 		// WebKit
+// 	if (event.wheelDeltaY) {
+// 		fov -= event.wheelDeltaY * 0.05;
+
+// 		// Opera / Explorer 9
+// 	}
+// 	else if (event.wheelDelta) {
+// 		fov -= event.wheelDelta * 0.05;
+
+// 		// Firefox
+// 	}
+// 	else if (event.detail) {
+// 		fov += event.detail * 1.0;
+// 	}
+
+// 	camera.updateProjectionMatrix();
+
+// }
+
+// var mouseX = 0,
+// 	mouseY = 0;
+
+// function onDocumentMouseMove(e) {
+// 	mouseX = 10 * (.5 * window.innerWidth - e.pageX);
+// 	mouseY = 10 * (.5 * window.innerHeight - e.pageY);
+// }
+
+
+function onWindowResize() {
+	var s = 1,
+		w = window.innerWidth,
+		h = window.innerHeight;
+	renderer.setSize(s * w, s * h);
+	camera.aspect = window.innerWidth / window.innerHeight;
+
+	camera.updateProjectionMatrix();
+	resizePass();
+	console.log("THREE.Window resized.")
 
 }
